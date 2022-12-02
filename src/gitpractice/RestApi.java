141463f0a36ff8702156f71055f677633288c79e
sam <Vuppula.Samyuktha@pacteraedge.com>
2022-12-02 10:23:42 +0530

    sam

---
+++ src/gitpractice/RestApi.java
@@ -0,0 +1,21 @@
+package gitpractice;
+
+import org.testng.annotations.Test;
+
+public class RestApi {
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+		System.out.println("postJira2");
+		System.out.println("postJira3");
+		
+	}
+
+@Test
+public void deleteTwitter()
+{
+	System.out.println("deleteTwitter");
+}
+
+}
